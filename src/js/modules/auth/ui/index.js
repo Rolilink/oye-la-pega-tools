d873e8203426c59a4eb4a38cce42a88a292e8acd
Rolando Perez <me@rolilink.com>
2017-05-28 16:56:25 -0500

    more cleanup

--- src/js/modules/auth/ui/index.js
+++ src/js/modules/auth/ui/index.js
@@ -1,10 +1,9 @@
 import EmailInput from './components/EmailInput';
 import LoginForm from './components/LoginForm';
 import LoginView from './components/LoginView';
-import LogoutButton from './components/LogoutButton';
 import PasswordInput from './components/PasswordInput';
 
 import LoginContainer from './containers/LoginContainer';
 
-export const components = { EmailInput, LoginForm, LoginView, LogoutButton, PasswordInput };
+export const components = { EmailInput, LoginForm, LoginView, PasswordInput };
 export const containers = { LoginContainer };
